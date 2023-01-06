900ca59bd6019ef2e3ad5b94bce0d55d99c17b87
Bugs Bunny <57536929+mamamia5x@users.noreply.github.com>
2023-01-06 01:38:32 +0000

    Add teams and make file

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 var json = require('./teams.json');
+var fs = require('fs');
+var request = require('request');
+var mime = require('mime-types');
+fs.writeFileSync("text.txt", "");
+var download = function(uri, filename, callback){
+  request.head(uri, function(err, res, body){
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
+    request(uri).pipe(fs.createWriteStream(filename+"."+mime.extension(res.headers['content-type']))).on('close', callback);
+  });
+};
+
+
 // console.log(json)
 for (var i of json.teams){
-  console.log(i.imgURL)
+  let dir = "./"+i.abbrev;
+  let text = "=".repeat(60)+"\n";
+  if (!fs.existsSync(dir)){
+    let val = i.region + " " + i.name;
+    let l = 60-val.length;
+    text += " ".repeat(Math.floor(l/2)) + val + " ".repeat(Math.ceil(l/2));
+    text += "\nImage url:\n"+i.imgURL+"\n";
+    val = "Folder: " + dir;
+    l = 60-val.length;
+    text += " ".repeat(Math.floor(l/2)) + val + " ".repeat(Math.ceil(l/2));
+    text += "\n";
+    fs.mkdirSync(dir);
+    fs.writeFileSync("text.txt", fs.readFileSync("text.txt")+text);
+    download(i.imgURL, dir+"/logo", a=>console.log('done'));
+  }
+  i.imgURL = "https://mamamia5x.github.io/BBGM-College/" + i.abbrev + "/" + fs.readdirSync(dir)[0];
 }
+fs.writeFileSync("College.json", JSON.stringify(json));
